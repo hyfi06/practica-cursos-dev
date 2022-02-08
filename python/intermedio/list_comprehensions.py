f98d5a910bb942487e519339e1c060517583b8be
Héctor Olvera Vital <hector.olvera@ciencias.unam.mx>
2022-02-07 22:41:50 -0600

    feat: add list comprehensions

--- python/intermedio/list_comprehensions.py
+++ python/intermedio/list_comprehensions.py
@@ -1,8 +1,5 @@
 def main():
-    squares = []
-    for i in range(1, 101):
-        if i % 3 != 0:
-            squares.append(i**2)
+    squares = [i**2 for i in range(1, 101) if i % 3 != 0]
 
     print(squares)
 
