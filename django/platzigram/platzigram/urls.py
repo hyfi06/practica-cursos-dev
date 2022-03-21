7e086f617d6a21696626a903c4a4129c01d78ff0
Héctor Olvera Vital <hector.olvera@ciencias.unam.mx>
2022-03-21 12:13:46 -0600

    chore: add Post model and registred in admin
    Signed-off-by: Héctor Olvera Vital <hector.olvera@ciencias.unam.mx>

--- django/platzigram/platzigram/urls.py
+++ django/platzigram/platzigram/urls.py
@@ -1,19 +1,9 @@
-"""platzigram URL Configuration
+"""Platzigram URLs module. """
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+# Django
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 from platzigram import views as local_views
 from posts import views as posts_views
@@ -25,4 +15,4 @@ urlpatterns = [
     path('sort/', local_views.sorted_numbers),
     path('admin/', admin.site.urls),
     path('posts/', posts_views.list_posts)
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- django/platzigram/posts/admin.py
+++ django/platzigram/posts/admin.py
@@ -1,3 +1,23 @@
+"""Post admin classes."""
+
+from re import search
 from django.contrib import admin
 
-# Register your models here.
+# Models
+from posts.models import Post
+
+
+@admin.register(Post)
+class ProfileAdmin(admin.ModelAdmin):
+    """Post admin."""
+    list_display = ('pk', 'title', 'user', 'created', 'modified')
+    list_display_links = ('pk', 'title')
+    search_fields = (
+        'title',
+        'user__username'
+        'user__email',
+    )
+    list_filter = (
+        'created',
+        'modified'
+    )
