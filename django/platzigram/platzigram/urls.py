306cd6563f3532cba8ac53ed9edcb3d413c4efe5
Héctor Olvera Vital <hector.olvera@ciencias.unam.mx>
2022-03-22 23:31:40 -0600

    feat: add new post form
    Signed-off-by: Héctor Olvera Vital <hector.olvera@ciencias.unam.mx>

--- django/platzigram/platzigram/urls.py
+++ django/platzigram/platzigram/urls.py
@@ -10,9 +10,10 @@ from users import views as users_views
 
 urlpatterns = [
     path('admin/', admin.site.urls,),
-    path('posts/', posts_views.list_posts, name='feed'),
+    path('', posts_views.list_posts, name='feed'),
+    path("post/new/", posts_views.create_posts, name="create_post"),
     path("users/login/", users_views.login_view, name="login"),
     path("users/logout/", users_views.logout_view, name="logout"),
     path('users/signup/', users_views.signup_view, name='signup'),
-    path('users/me/profile', users_views.update_profile,name='update_profile')
+    path('users/me/profile', users_views.update_profile, name='update_profile')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- django/platzigram/users/forms.py
+++ django/platzigram/users/forms.py
@@ -1,8 +1,34 @@
 """Forms users."""
 
 # Django
-
 from django import forms
+
+# Models
+from django.contrib.auth.models import User
+
+
+class SignupForm(forms.Form):
+    username = forms.CharField(min_length=4, max_length=50)
+    passwd = forms.CharField(max_length=70, widget=forms.PasswordInput())
+    passwd_confirmation = forms.CharField(
+        max_length=70, widget=forms.PasswordInput())
+    first_name = forms.CharField(min_length=2, max_length=50)
+    last_name = forms.CharField(min_length=2, max_length=50)
+    email = forms.CharField(
+        min_length=6,
+        max_length=70,
+        widget=forms.EmailInput()
+    )
+
+    def clean_username(self):
+        """Username must be unique"""
+        username = self.cleaned_data['username']
+        username_taken = User.objects.filter(username=username).exists()
+        
+        if username_taken:
+            raise forms.ValidationError('Username is already in use.')
+    
+        return username
 
 
 class ProfileForm(forms.Form):
