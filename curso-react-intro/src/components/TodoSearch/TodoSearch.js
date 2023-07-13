6fb35a4b9eb47f4039d521f5abd2022914e79baa
Héctor Olvera Vital <hector.olvera@ciencias.unam.mx>
2023-07-13 17:17:49 -0600

    chore: agregar estado a TodoSearch

--- curso-react-intro/src/components/TodoSearch/TodoSearch.js
+++ curso-react-intro/src/components/TodoSearch/TodoSearch.js
@@ -1,7 +1,19 @@
 import "./TodoSearch.css";
+import React from "react";
 
 function TodoSearch() {
-  return <input className="TodoSearch" type="search" placeholder="Cortar cebolla" />;
+  const [searchValue, setSearchValue] = React.useState("");
+  return (
+    <input
+      className="TodoSearch"
+      type="search"
+      placeholder="Cortar cebolla"
+      value={searchValue}
+      onChange={(event) => {
+        setSearchValue(event.target.value);
+      }}
+    />
+  );
 }
 
 export { TodoSearch };
