b03013cae88b4156f4416f2653a433ff3aceafa4
Héctor Olvera Vital <hector.olvera@ciencias.unam.mx>
2025-07-23 19:06:40 -0600

    feat: insert records
    Signed-off-by: Héctor Olvera Vital <hector.olvera@ciencias.unam.mx>

--- rust-backend/rust-blog/src/main.rs
+++ rust-backend/rust-blog/src/main.rs
@@ -1,3 +1,36 @@
+#[macro_use]
+extern crate diesel;
+use dotenvy::dotenv;
+use std::env;
+
+use diesel::pg::PgConnection;
+use diesel::prelude::*;
+
+pub mod models;
+pub mod schema;
+
 fn main() {
-    println!("Hello, world!");
+    dotenv().ok();
+    let db_url = env::var("DATABASE_URL").expect("db url not found");
+
+    let mut conn = PgConnection::establish(&db_url).expect("DB no connected");
+
+    use self::models::{NewPost, Post};
+    use self::schema::posts;
+    use self::schema::posts::dsl::*;
+
+    let new_post = NewPost {
+        title: "Mi tercer post",
+        body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi libero odio, commodo a consectetur ut, ultrices nec libero. Integer suscipit mi nunc, ut vulputate felis lacinia nec. Praesent nec sem turpis. Maecenas aliquam non nunc ut bibendum. Donec eget condimentum metus, eu venenatis enim. Cras porttitor facilisis nisl, at pretium elit facilisis a. Duis ultricies et lacus eu bibendum. Curabitur quis enim convallis, gravida orci eu, eleifend sapien.",
+        slug: "tercer-post",
+    };
+    let _post: Post = diesel::insert_into(posts::table)
+        .values(&new_post)
+        .get_result(&mut conn)
+        .expect("Insert post error");
+
+    let posts_list = posts.limit(1).load::<Post>(&mut conn).expect("Query error");
+    for post in posts_list {
+        println!("{:?}", post);
+    }
 }
---
+++ rust-backend/rust-blog/src/models.rs
@@ -0,0 +1,19 @@
+use serde;
+
+#[derive(Queryable, serde::Serialize, serde::Deserialize, Clone, Debug)]
+pub struct Post {
+    pub id: i32,
+    pub title: String,
+    pub slug: String,
+    pub body: String,
+}
+
+use super::schema::posts;
+
+#[derive(Insertable)]
+#[table_name="posts"]
+pub struct NewPost<'a> {
+    pub title: &'a str,
+    pub slug: &'a str,
+    pub body: &'a str,
+}
