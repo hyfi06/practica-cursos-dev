a08147d528456b8072768829bf13b0a045cd0fb4
Héctor Olvera Vital <hector.olvera@ciencias.unam.mx>
2025-07-25 20:20:52 -0600

    feat: add tera
    Signed-off-by: Héctor Olvera Vital <hector.olvera@ciencias.unam.mx>

--- rust-backend/rust-blog/src/main.rs
+++ rust-backend/rust-blog/src/main.rs
@@ -1,13 +1,12 @@
-#[macro_use]
 extern crate diesel;
 use dotenvy::dotenv;
 use std::env;
 
 use actix_web::{App, HttpResponse, HttpServer, Responder, get, post, web};
-
 use diesel::pg::PgConnection;
 use diesel::prelude::*;
 use diesel::r2d2::{self, ConnectionManager, Pool};
+use tera::{Context, Tera};
 
 pub mod models;
 pub mod schema;
@@ -17,12 +16,20 @@ use schema::posts::dsl::*;
 
 pub type DbPool = r2d2::Pool<ConnectionManager<PgConnection>>;
 
+#[get("/tera-test")]
+async fn tera_test(template_manager: web::Data<Tera>) -> impl Responder {
+    let ctx = Context::new();
+
+    HttpResponse::Ok()
+        .content_type("text/html")
+        .body(template_manager.render("index.html", &ctx).unwrap())
+}
 #[get("/")]
 async fn index(pool: web::Data<DbPool>) -> impl Responder {
     let mut conn = pool.get().expect("Problemas al traer la base de datos");
     match web::block(move || posts.load::<Post>(&mut conn)).await {
         Ok(data) => HttpResponse::Ok().json(data.unwrap()),
-        Err(err) => HttpResponse::Ok().body("Hubo un error"),
+        Err(_) => HttpResponse::Ok().body("Hubo un error"),
     }
 }
 
@@ -45,11 +52,14 @@ async fn main() -> std::io::Result<()> {
         .build(conn)
         .expect("Could not build connection pool");
 
+    let tera = Tera::new(concat!(env!("CARGO_MANIFEST_DIR"), "/templates/**/*")).unwrap();
     HttpServer::new(move || {
         App::new()
             .app_data(web::Data::new(pool.clone()))
+            .app_data(web::Data::new(tera.clone()))
             .service(index)
             .service(new_post)
+            .service(tera_test)
     })
     .bind(("127.0.0.1", 9900))?
     .run()
--- rust-backend/rust-blog/src/models.rs
+++ rust-backend/rust-blog/src/models.rs
@@ -13,7 +13,7 @@ pub struct Post {
 }
 
 #[derive(Insertable)]
-#[table_name = "posts"]
+#[diesel(table_name = posts)]
 pub struct NewPost<'a> {
     pub title: &'a str,
     pub slug: &'a str,
