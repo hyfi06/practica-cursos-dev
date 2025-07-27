8786df3528e330bf7d6bd02f45ba7156d9410253
Héctor Olvera Vital <hector.olvera@ciencias.unam.mx>
2025-07-27 13:57:24 -0600

    feat: dokerization
    Signed-off-by: Héctor Olvera Vital <hector.olvera@ciencias.unam.mx>

--- rust-backend/rust-blog/src/main.rs
+++ rust-backend/rust-blog/src/main.rs
@@ -19,7 +19,6 @@ pub type DbPool = r2d2::Pool<ConnectionManager<PgConnection>>;
 #[get("/")]
 async fn index(pool: web::Data<DbPool>, tmplt_mngr: web::Data<Tera>) -> impl Responder {
     let mut conn = pool.get().expect("Problemas al traer la base de datos");
-
     match web::block(move || posts.load::<Post>(&mut conn)).await {
         Ok(data) => {
             let mut ctx = Context::new();
@@ -72,14 +71,14 @@ async fn new_post(pool: web::Data<DbPool>, item: web::Json<NewPostHandler>) -> i
 async fn main() -> std::io::Result<()> {
     dotenv().ok();
     let db_url = env::var("DATABASE_URL").expect("db url not found");
-    let port= env::var("PORT").expect("Port not found");
+    let port = env::var("PORT").expect("Port not found");
     let port: u16 = port.parse().unwrap_or(9900);
     let conn = ConnectionManager::<PgConnection>::new(db_url);
     let pool: DbPool = Pool::builder()
         .build(conn)
         .expect("Could not build connection pool");
-
     let tera = Tera::new(concat!(env!("CARGO_MANIFEST_DIR"), "/templates/**/*")).unwrap();
+    println!("listening on IPv4 address \"0.0.0.0\", port {}", port);
     HttpServer::new(move || {
         App::new()
             .app_data(web::Data::new(pool.clone()))
@@ -88,7 +87,7 @@ async fn main() -> std::io::Result<()> {
             .service(new_post)
             .service(get_post)
     })
-    .bind(("127.0.0.1", port))?
+    .bind(("0.0.0.0", port))?
     .run()
     .await
 }
